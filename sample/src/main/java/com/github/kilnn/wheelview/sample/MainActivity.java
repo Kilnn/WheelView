727881e488090b207a5cf8f3435de7982a8db27a
Kilnn <kilnn@qq.com>
2022-01-25 21:42:55 +0800

    [opt]:更新gradle

--- sample/src/main/java/com/github/kilnn/wheelview/sample/MainActivity.java
+++ sample/src/main/java/com/github/kilnn/wheelview/sample/MainActivity.java
@@ -1,7 +1,8 @@
 package com.github.kilnn.wheelview.sample;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.github.kilnn.wheelview.WheelView;
 import com.github.kilnn.wheelview.adapters.NumericWheelAdapter;
